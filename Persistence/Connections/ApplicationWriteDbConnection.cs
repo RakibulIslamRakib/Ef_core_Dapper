138f5e521e9e93f761cafe116f11d543c1fb9722
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2024-09-09 16:07:49 +0600

    working code for develop

--- Persistence/Connections/ApplicationWriteDbConnection.cs
+++ Persistence/Connections/ApplicationWriteDbConnection.cs
@@ -6,26 +6,26 @@ namespace Persistence.Connections
 {
     public class ApplicationWriteDbConnection : IApplicationWriteDbConnection
     {
-        private readonly IApplicationDbContext context;
+        private readonly IApplicationDbContext _context;
         public ApplicationWriteDbConnection(IApplicationDbContext context)
         {
-            this.context = context;
+            _context = context;
         }
         public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, CancellationToken cancellationToken = default)
         {
-            return await context.Connection.ExecuteAsync(sql, param, transaction);
+            return await _context.Connection.ExecuteAsync(sql, param, transaction);
         }
         public async Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null, CancellationToken cancellationToken = default)
         {
-            return (await context.Connection.QueryAsync<T>(sql, param, transaction)).AsList();
+            return (await _context.Connection.QueryAsync<T>(sql, param, transaction)).AsList();
         }
         public async Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null, IDbTransaction transaction = null, CancellationToken cancellationToken = default)
         {
-            return await context.Connection.QueryFirstOrDefaultAsync<T>(sql, param, transaction);
+            return await _context.Connection.QueryFirstOrDefaultAsync<T>(sql, param, transaction);
         }
         public async Task<T> QuerySingleAsync<T>(string sql, object param = null, IDbTransaction transaction = null, CancellationToken cancellationToken = default)
         {
-            return await context.Connection.QuerySingleAsync<T>(sql, param, transaction);
+            return await _context.Connection.QuerySingleAsync<T>(sql, param, transaction);
         }
     }
 }
